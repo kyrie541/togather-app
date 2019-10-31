c35fde6d61ecee70e40949beac0f096a52e44c6e
Kyrie Koh <kohkimkhang@yahoo.com>
2019-10-31 09:40:52 +0800

    minor change

--- togather_server/models/index.js
+++ togather_server/models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/warbler", {
+mongoose.connect("mongodb://localhost/togather", {
   keepAlive: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
